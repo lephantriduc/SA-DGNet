4f9f753a2fe817a88b06a5912c35c8fa4535f9b0
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2025-10-08 13:30:19 +0700

    feat: update dependencies

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     install_requires=[
-        "torch==1.12.1",
-        "pycox==0.2.3",
-        "scikit-learn==1.2.2",
+        "torch>=.12.1",
+        "pycox>=0.2.3",
+        "scikit-learn>=1.2.2",
     ],
 )
